6df0c9586cd83c000c8fc0e51f704de1ae09ac11
Shmully Wolfson <shmullywolfson@gmail.com>
2016-10-28 13:58:15 -0400

    edit #play

--- lib/tic_tac_toe_cli.rb
+++ lib/tic_tac_toe_cli.rb
@@ -54,7 +54,7 @@ class TicTacToeCLI
       when "y".downcase
         self.call
       when "n".downcase
-        puts "A strange game. The only winning move is not to play. How about a nice game of chess?!"
+        puts "\"A strange game. The only winning move is not to play. How about a nice game of chess?!\""
       when "exit"
         exit
       else
